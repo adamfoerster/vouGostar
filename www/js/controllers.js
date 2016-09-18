5b0b1c1d3f99438bcaaceb76d6644fa9346fb492
Adam Foerster <adam.foerster@gmail.com>
2016-09-18 19:16:21 -0300

    Mudei a forma de busca de títulos e miniaturas. Primeiramente procura localmente e se não encontrar busca na API.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,16 +1,42 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope, Filmes, $ionicSideMenuDelegate) {
-	$scope.filmes_vaigostar = Filmes.listar(true);
+.controller('DashCtrl', function($scope, Filmes, Globais, $ionicSideMenuDelegate, $http) {
+    // listar todos os filme que a pessoa vai gostar e verificar se tem
+    // thumbnail e titulo. se não puxa da api
+    $scope.filmes_vaigostar = Filmes.listar(true);
+    for (var i = 0; i < $scope.filmes_vaigostar.length; i++){
+        if ($scope.filmes_vaigostar[i].thumbnail == undefined || $scope.filmes_vaigostar[i].titulo == undefined){
+            var req = Globais.getReq($scope.filmes_vaigostar[i].id);
+        	$http(req).then(function successCallback(response){
+                for (var i = 0; i < $scope.filmes_vaigostar.length; i++) {
+                    if ($scope.filmes_vaigostar[i].id == response.data.imdbID){
+                        $scope.filmes_vaigostar[i].thumbnail = response.data.Poster;
+                        $scope.filmes_vaigostar[i].titulo = response.data.Title;
+                    }
+                }
+        	});
+        }
+    }
 	$scope.filmes_nvaigostar = Filmes.listar(false);
-
+    for (var i = 0; i < $scope.filmes_nvaigostar.length; i++){
+        if ($scope.filmes_nvaigostar[i].thumbnail == undefined || $scope.filmes_nvaigostar[i].titulo == undefined){
+            var req = Globais.getReq($scope.filmes_nvaigostar[i].id);
+        	$http(req).then(function successCallback(response){
+                for (var i = 0; i < $scope.filmes_nvaigostar.length; i++) {
+                    if ($scope.filmes_nvaigostar[i].id == response.data.imdbID){
+                        $scope.filmes_nvaigostar[i].thumbnail = response.data.Poster;
+                        $scope.filmes_nvaigostar[i].titulo = response.data.Title;
+                    }
+                }
+        	});
+        }
+    }
 	$scope.txt_vaigostar = "Você vai gostar!";
 	$scope.txt_nvaigostar = "Você vai detestar!";
 
 	$scope.toggleLeft = function() {
 		$ionicSideMenuDelegate.toggleLeft();
 	};
-
 })
 
 .controller('AccountCtrl', function($scope, Omdb, $http, Globais) {
@@ -42,29 +68,52 @@ angular.module('starter.controllers', [])
 	}
 })
 
-.controller('SearchCtrl', function($scope, Omdb, $http, Globais) {
-
-	$scope.getFilme = function(titulo) {
-		var req = {
-			 method: 'GET',
-			 url: Globais.getUrl() ,
-			 headers: {
-			   'Content-Type': 'application/json',
-			 },
-			 params: {
-				't' : titulo,
-				'plot' : 'short',
-				'r' : 'json',
-			 }
-		}
-		$http(req).then(function successCallback(response){
-			$scope.filme = response.data;
-		});
+.controller('SearchCtrl', function($scope, $http, Globais, Omdb) {
+    $scope.getFilme = function(titulo) {
+        $scope.titulo = '';
+        console.log($scope.titulo);
+        if (titulo.length > 3){
+            $http(Globais.getReqSearch(titulo)).then(function successCallback(response){
+    			$scope.filmes = response.data.Search;
+                console.log(response);
+    		});
+        }
 	}
 })
 
-.controller('FilmeCtrl', function($scope, $stateParams, Filmes) {
-	$scope.filme = Filmes.get($stateParams.filmeId);
+.controller('FilmeCtrl', function($scope, $stateParams, Filmes, Omdb, $http, Globais) {
+	// $scope.filme = Filmes.get($stateParams.filmeId);
+
+	var req = {
+		 method: 'GET',
+		 url: Globais.getUrl() ,
+		 headers: {
+		   'Content-Type': 'application/json',
+		 },
+		 params: {
+			'i' : $stateParams.filmeId,
+			'plot' : 'short',
+			'r' : 'json',
+		 }
+	}
+	$http(req).then(function successCallback(response){
+		$scope.filme = response.data;
+        var local = Filmes.get($scope.filme.imdbID);
+        console.log(local);
+        if (local.titulo != undefined){
+            $scope.filme.Title = local.titulo;
+        }
+        if (local.info != undefined){
+            $scope.filme.Plot = local.info;
+        }
+        if (local.vaiGostar != undefined){
+            $scope.filme.vaiGostar = local.vaiGostar;
+        }
+        if (local.capa != undefined){
+            //$scope.filme.Poster = 'img/' + local.capa;
+        }
+	});
+
 })
 
 .controller('HorariosCtrl', function($scope, $stateParams, Filmes) {
--- www/js/services.js
+++ www/js/services.js
@@ -2,10 +2,26 @@ angular.module('starter.services', [])
 
 .factory('Filmes', function() {
   var filmes = [{
-    id: 1,
-    titulo: 'Star Trek Sem Fronteiras',
-    thumbnail: 'startrek.jpg',
-    capa: 'capa_startrek.jpg',
+    id: 'tt1540011',
+    titulo: 'Bruxa de Blair',
+    vaiGostar: false,
+  }, {
+    id: 'tt2709768',
+    titulo: 'Pets - A vida secreta dos bichos',
+    thumbnail: 'img/pets.jpg',
+    vaiGostar: true,
+  }, {
+    id: 'tt4160708',
+    titulo: 'O homem nas trevas',
+    vaiGostar: false,
+  }, {
+    id: 'tt5475002',
+    titulo: 'Um namorado para minha esposa',
+    thumbnail: 'img/namorado.jpg',
+    vaiGostar: false,
+  }, {
+    id: 'tt2660888',
+    titulo: 'Star Trek - Sem Fronteiras',
     vaiGostar: true,
     info: "Star Trek Beyond é um filme norte-americano de 2016 dirigido por Justin Lin e escrito por Simon Pegg e Doug Jung. É o décimo terceiro longa-metragem da franquia Star Trek e o terceiro estrelado pelo novo elenco na série reboot. Na história, a tripulação da USS Enterprise é atacada e presa por uma espécia alienígena desconhecida, precisando encontrar um modo de fugir e enfrentar um inimigo que odeia a Federação dos Planetas Unidos.",
     horarios: [{
@@ -16,26 +32,15 @@ angular.module('starter.services', [])
       sessoes: "<b>Sala 2</b><br />* Quinta a quarta (legendado): 21h40<br /><b>Preço:</b><br/>* Segunda e terça: R$ 20 (até 17h) <br/>* Segunda e terça: R$ 22 (após 17h)<br />* Quarta: R$ 19<br/>* Quinta, sexta, sábado, domingo e feriado: R$ 24 (até 17h)<br/>* Quinta, sexta, sábado, domingo e feriado: R$ 27 (após 17h) ",
     }, ],
   }, {
-    id: 4,
+    id: 'tt1386697',
     titulo: 'Esquadrão Suicida',
-    thumbnail: 'suicide.jpg',
+    thumbnail: 'img/suicide.jpg',
     vaiGostar: true,
   }, {
-    id: 5,
-    titulo: 'Ben Hur',
-    thumbnail: 'benhur.jpg',
+    id: 'tt1355631',
+    titulo: 'Conexão Escobar',
     vaiGostar: true,
-  }, {
-    id: 2,
-    titulo: 'A vida secreta dos bichos',
-    thumbnail: 'pets.jpg',
-    vaiGostar: false,
-  }, {
-    id: 3,
-    titulo: 'Um namorado pra minha mulher',
-    thumbnail: 'namorado.jpg',
-    vaiGostar: false,
-  }, ];
+  }];
 
   return {
     listar: function(vaiGostar) {
@@ -64,11 +69,40 @@ angular.module('starter.services', [])
 })
 
 .factory('Globais', function() {
+  var url = "http://www.omdbapi.com/";
 
   return {
     getUrl: function() {
-        return "http://www.omdbapi.com/";
+      return url;
     },
+    getReq: function(id) {
+      return {
+        method: 'GET',
+        url: url,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        params: {
+          'i': id,
+          'plot': 'short',
+          'r': 'json',
+        }
+      }
+  },
+    getReqSearch: function(t){
+        return {
+			 method: 'GET',
+			 url: url,
+			 headers: {
+			   'Content-Type': 'application/json',
+			 },
+			 params: {
+				's' : t + '%',
+				'plot' : 'short',
+				'r' : 'json',
+			 }
+		}
+    }
   }
 })
 
